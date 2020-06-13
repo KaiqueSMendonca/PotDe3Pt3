e20a3be585913d274419fe6f6144fa3828f28862
KaiqueSMendonca <kaique.mendonca@etec.sp.gov.br>
2020-06-13 14:26:31 -0300

    potencias de 3 parte 3

---
+++ src/potenciasde3pt3/PotenciasDe3Pt3.java
@@ -0,0 +1,14 @@
+package potenciasde3pt3;
+
+public class PotenciasDe3Pt3 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        
+        for (i=0; i<=15; i++) {
+            int r = (int) Math.pow(3, i);
+            System.out.println(r+", ");
+        }
+    }
+    
+}
